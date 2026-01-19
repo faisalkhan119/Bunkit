affe5a1fd3352ba219c4de999879d7a89a8b38d3
faisalkhan119 <faisal.khan1192519@gmail.com>
2026-01-20 00:32:07 +0530

    Fix screenshot attendance calculation >100% bug

--- debug_find.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-findstr /n "submitDailyLog" d:\student\index.html > found_submit.txt
-findstr /n "dailyLogModal.innerHTML" d:\student\index.html >> found_submit.txt
-findstr /n "saveDailyLog" d:\student\index.html >> found_submit.txt
-findstr /n "setupModals" d:\student\index.html >> found_submit.txt
